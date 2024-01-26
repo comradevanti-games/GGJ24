6c25b4bd2cb7192b34dd9baec41d8b29655bec01
ComradeVanti <kingvanti650@gmail.com>
2024-01-26 22:42:58 +0100

    feat: add more domain types

---
+++ Assets/Code/Runtime/Performance.cs
@@ -0,0 +1,10 @@
+﻿namespace Dev.ComradeVanti.GGJ24
+{
+    /// <summary>
+    /// An act in progress. Contains information about the current score
+    /// and stats about the player.
+    /// </summary>
+    public class Performance
+    {
+    }
+}
