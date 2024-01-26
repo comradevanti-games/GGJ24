0ba0f1b511c9b25f55ee26257193683bd4e9b071
ComradeVanti <kingvanti650@gmail.com>
2024-01-26 22:24:58 +0100

    feat: add inventory class

---
+++ Assets/Code/Runtime/Inventory.cs
@@ -0,0 +1,9 @@
+﻿namespace Dev.ComradeVanti.GGJ24
+{
+    /// <summary>
+    /// Contains information about what props the player brings into the act.
+    /// </summary>
+    public class Inventory
+    {
+    }
+}
