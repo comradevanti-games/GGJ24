a0aebc8a216b8d8b77d18d01bc52fd9bc2c6fa34
ComradeVanti <kingvanti650@gmail.com>
2024-01-26 22:21:32 +0100

    feat: add humor-types

---
+++ Assets/Code/Runtime/HumorTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace Dev.ComradeVanti.GGJ24
+{
+    /// <summary>
+    /// The different types of humor that people can find funny.
+    /// </summary>
+    public enum HumorTypes
+    {
+        Schadenfreude,
+        Absurdism,
+        Childish
+    }
+}
