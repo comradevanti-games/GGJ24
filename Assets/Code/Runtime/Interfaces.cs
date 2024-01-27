7b33e7c5953801a5b4aeb2746986ccff7cf5f46d
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-27 12:04:45 +0100

    add player phase prop

--- Assets/Code/Runtime/Interfaces.cs
+++ Assets/Code/Runtime/Interfaces.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Dev.ComradeVanti.GGJ24.Player;
 using UnityEngine;
 
 namespace Dev.ComradeVanti.GGJ24
@@ -12,7 +13,7 @@ namespace Dev.ComradeVanti.GGJ24
 
     public interface IPhaseKeeper
     {
-        public record PhaseChangedArgs();
+        public record PhaseChangedArgs(PlayerPhase NewPhase);
 
 
         public event Action<PhaseChangedArgs> PhaseChanged;
