459e8c3bda902d09eaa61b847b481455787ab0aa
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 12:11:18 +0100

    feat: impelement phase-keeper
    commit 0b3d7bb9ab7f69627fc4df33934ed17b00adf8e6
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 12:10:49 2024 +0100
    
        feat: implement IPhaseKeeper
    
    commit 8da89b2e42db3f2c7564ce69f70625ff88339987
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 12:10:15 2024 +0100
    
        feat: add phase-keeper script

---
+++ Assets/Code/Runtime/PhaseKeeper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dev.ComradeVanti.GGJ24
+{
+    public class PhaseKeeper : MonoBehaviour, IPhaseKeeper
+    {
+        public event Action<IPhaseKeeper.PhaseChangedArgs> PhaseChanged;
+    }
+}
