13c60f4c72a340d379e152c42c93bc2eac6be358
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 15:39:04 +0100

    feat: empty inventory

--- Assets/Code/Runtime/Inventory.cs
+++ Assets/Code/Runtime/Inventory.cs
@@ -1,8 +1,13 @@
-﻿namespace Dev.ComradeVanti.GGJ24
+﻿using System.Collections.Immutable;
+
+namespace Dev.ComradeVanti.GGJ24
 {
     /// <summary>
     /// Contains information about what props the player brings into the act.
     /// </summary>
-    public record Inventory();
-    
+    public record Inventory(IImmutableList<IProp> Props)
+    {
+        public static readonly Inventory Empty =
+            new Inventory(ImmutableList<IProp>.Empty);
+    }
 }
