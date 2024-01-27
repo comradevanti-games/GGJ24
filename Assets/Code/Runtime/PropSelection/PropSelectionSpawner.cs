c22484682c2c7d3ce3474be740513a22d798ab3f
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-27 19:58:26 +0100

    add despawning of propselection

--- Assets/Code/Runtime/PropSelection/PropSelectionSpawner.cs
+++ Assets/Code/Runtime/PropSelection/PropSelectionSpawner.cs
@@ -49,7 +49,11 @@ namespace Dev.ComradeVanti.GGJ24.PropSelection {
 		private void OnPhaseChanged(IPhaseKeeper.PhaseChangedArgs e) {
 
 			if (e.NewPhase == PlayerPhase.PropSelection) {
+				gameObject.SetActive(true);
 				DisplayProps();
+			}
+			else {
+				gameObject.SetActive(false);
 			}
 
 		}
@@ -61,7 +65,8 @@ namespace Dev.ComradeVanti.GGJ24.PropSelection {
 			for (int i = 0; i <= AllProps.Count - 1; i++) {
 
 				var newProp = Instantiate(propSelectionPrefab,
-					new Vector3((propSelectionOrigin.x + (8.5f * i)), propSelectionOrigin.y, propSelectionOrigin.z), Quaternion.identity);
+					new Vector3((propSelectionOrigin.x + (8.5f * i)), propSelectionOrigin.y, propSelectionOrigin.z), Quaternion.identity,
+					transform);
 
 				newProp.GetComponent<SpriteRenderer>().sprite = AllProps[i].Thumbnail;
 				PropPositions.Add(newProp.transform.position);
