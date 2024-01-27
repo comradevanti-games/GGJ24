c1a5fbef6d827198fa84cdf893d957879fb515e6
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 13:56:52 +0100

    feat: build slots

--- Assets/Code/Runtime/LiveStageKeeper.cs
+++ Assets/Code/Runtime/LiveStageKeeper.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using UnityEngine;
 
 namespace Dev.ComradeVanti.GGJ24
 {
@@ -17,7 +22,47 @@ namespace Dev.ComradeVanti.GGJ24
 
         [SerializeField] private Transform firstSlotTransform;
 
+        private IPropBuilder propBuilder = null!;
+        private GameObject?[] liveProps = new GameObject?[Stage.SlotsPerStage];
+
+
         public Vector3? TryGetPositionForSlot(int slotIndex) =>
             firstSlotTransform.position + stageDirection * UnitsPerSlot;
+
+
+        private void ClearSlot(int slotIndex)
+        {
+            var liveProp = liveProps[slotIndex];
+            if (liveProp != null) Destroy(liveProp);
+        }
+
+        private void BuildSlot(int slotIndex, IProp prop)
+        {
+            var liveProp = propBuilder.BuildProp(prop, slotIndex);
+            liveProps[slotIndex] = liveProp;
+        }
+
+        private void BuildStage(Stage stage)
+        {
+            for (var slotIndex = 0; slotIndex < Stage.SlotsPerStage; slotIndex++)
+            {
+                ClearSlot(slotIndex);
+                var prop = stage.Props[slotIndex];
+                if (prop == null) continue;
+
+                BuildSlot(slotIndex, prop);
+            }
+        }
+
+        private void OnStageChanged(IStageKeeper.StageChangedArgs args)
+        {
+            BuildStage(args.NewStage);
+        }
+
+        private void Awake()
+        {
+            propBuilder = Singletons.Require<IPropBuilder>();
+            Singletons.Require<IStageKeeper>().StageChanged += OnStageChanged;
+        }
     }
 }
