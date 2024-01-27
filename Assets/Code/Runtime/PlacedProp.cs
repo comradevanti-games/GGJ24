135e94ccf169081a8e908875ca16b5e08e16af74
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 10:49:48 +0100

    Squashed commit of the following:
    commit fef617b9065b5f4feabc011bdb19446916b198d1
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 10:49:39 2024 +0100
    
        refactor: rename constant
    
    commit f7d7eec63a152d1dba88aeb9437c38f0859f0084
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 10:49:14 2024 +0100
    
        refactor: rename functions
    
    commit 977c692c50d3f8a7cc54ab9084bd6ed42d451a11
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 10:48:48 2024 +0100
    
        feat: update prop
    
    commit 97325036d421423e61b17426228d3fe6967d4a82
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 10:46:38 2024 +0100
    
        feat: clear prop
    
    commit 8075e6384936c80ab77ca694c446bf27265d73ba
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 10:46:04 2024 +0100
    
        feat: place props
    
    commit 87c9f379377a65f9ba1083f39ff0d2857a7f3339
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 10:39:40 2024 +0100
    
        feat: empty stage
    
    commit 3c1401bcc627547480513020b26ba6c77dd19d25
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 10:37:14 2024 +0100
    
        feat: stage has placed-prop array

---
+++ Assets/Code/Runtime/PlacedProp.cs
@@ -0,0 +1,7 @@
+﻿namespace Dev.ComradeVanti.GGJ24
+{
+    /// <summary>
+    /// Contains information about a prop that is placed on a stage.
+    /// </summary>
+    public record PlacedProp(IProp Prop);
+}
--- Assets/Code/Runtime/Stage.cs
+++ Assets/Code/Runtime/Stage.cs
@@ -1,9 +1,63 @@
-﻿namespace Dev.ComradeVanti.GGJ24
+﻿#nullable enable
+
+using System;
+using System.Collections.Immutable;
+
+namespace Dev.ComradeVanti.GGJ24
 {
     /// <summary>
     /// Includes information about the currently placed props.
     /// </summary>
-    public class Stage
+    /// <param name="PlacedProps">The props placed on this stage. The
+    /// length of this array should always be <see cref="SlotsPerStage"/>.
+    /// </param>
+    public record Stage(ImmutableArray<PlacedProp?> PlacedProps)
     {
+        /// <summary>
+        /// The fixed amount of slots per stage.
+        /// </summary>
+        public const int SlotsPerStage = 20;
+
+
+        public static readonly Stage Empty = new Stage(
+            new PlacedProp?[SlotsPerStage].ToImmutableArray());
+
+
+        private static bool IsValidSlotIndex(int slotIndex) =>
+            slotIndex is >= 0 and < SlotsPerStage;
+
+        public static Stage? TryPlaceProp(Stage stage, int slotIndex, IProp prop)
+        {
+            if (!IsValidSlotIndex(slotIndex)) return null;
+
+            var placedProp = new PlacedProp(prop);
+            return stage with
+            {
+                PlacedProps = stage.PlacedProps.SetItem(slotIndex, placedProp)
+            };
+        }
+
+        public static Stage? TryClearProp(Stage stage, int slotIndex)
+        {
+            if (!IsValidSlotIndex(slotIndex)) return null;
+
+            return stage with
+            {
+                PlacedProps = stage.PlacedProps.SetItem(slotIndex, null)
+            };
+        }
+
+        public static Stage? TryUpdateProp(Stage stage, int slotIndex, Func<PlacedProp, PlacedProp> updateF)
+        {
+            if (!IsValidSlotIndex(slotIndex)) return null;
+
+            var prop = stage.PlacedProps[slotIndex];
+            if (prop == null) return stage;
+
+            return stage with
+            {
+                PlacedProps = stage.PlacedProps.SetItem(slotIndex, updateF(prop))
+            };
+        }
     }
 }
