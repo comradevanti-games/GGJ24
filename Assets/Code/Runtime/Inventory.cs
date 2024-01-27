65b63b15ac188aaa0e990f5c64aadd0106ca7eb0
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 20:31:34 +0100

    feat: max inventory size

--- Assets/Code/Runtime/Inventory.cs
+++ Assets/Code/Runtime/Inventory.cs
@@ -7,12 +7,18 @@ namespace Dev.ComradeVanti.GGJ24
     /// </summary>
     public record Inventory(IImmutableList<IProp> Props)
     {
+        public const int MaxItemCount = 5;
+
+
         public static readonly Inventory Empty =
             new Inventory(ImmutableList<IProp>.Empty);
 
 
-        public static Inventory Add(Inventory inventory, IProp prop) =>
-            inventory with {Props = inventory.Props.Add(prop)};
+        public static Inventory Add(Inventory inventory, IProp prop)
+        {
+            if (inventory.Props.Count == MaxItemCount) return inventory;
+            return inventory with {Props = inventory.Props.Add(prop)};
+        }
 
         public static Inventory Remove(Inventory inventory, IProp prop) =>
             inventory with {Props = inventory.Props.Remove(prop)};
