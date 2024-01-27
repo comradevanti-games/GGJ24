4e240f3b777f0fe0252ad0fc26264306f45eff1c
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-27 18:40:40 +0100

    add prop selection spawn

--- Assets/Code/Runtime/PropSelection/PropSelectionSpawner.cs
+++ Assets/Code/Runtime/PropSelection/PropSelectionSpawner.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using Dev.ComradeVanti.GGJ24.Player;
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
@@ -23,14 +22,13 @@ namespace Dev.ComradeVanti.GGJ24.PropSelection {
 		private List<IProp> AllProps { get; set; }
 		private IPhaseKeeper PhaseKeeper { get; set; }
 		private int HoveredPropID { get; set; }
-		private List<PropSelectionAsset> SelectableProps { get; set; }
 
 #endregion
 
 #region Methods
 
-		private async void Awake() {
-			AllProps = (await PropIO.LoadAllAsync()).ToList();
+		private void Awake() {
+			AllProps = PropIO.LoadAll().ToList();
 			PhaseKeeper = Singletons.Require<PhaseKeeper>();
 			PhaseKeeper.PhaseChanged += OnPhaseChanged;
 			FindAnyObjectByType<InputHandler>().PropChoosingInputPerformed += OnPropChoosingInputPerformed;
@@ -38,7 +36,6 @@ namespace Dev.ComradeVanti.GGJ24.PropSelection {
 		}
 
 		private void OnPhaseChanged(IPhaseKeeper.PhaseChangedArgs e) {
-
 			if (e.NewPhase == PlayerPhase.PropSelection) {
 				DisplayProps();
 			}
@@ -47,13 +44,10 @@ namespace Dev.ComradeVanti.GGJ24.PropSelection {
 
 		private void DisplayProps() {
 
-			SelectableProps = new List<PropSelectionAsset>();
-
 			for (int i = 0; i <= AllProps.Count; i++) {
 
-				SelectableProps.Add(Instantiate(propSelectionPrefab,
-						new Vector3((propSelectionOrigin.x + (1 * i)), propSelectionOrigin.y, propSelectionOrigin.z), Quaternion.identity)
-					.GetComponent<PropSelectionAsset>());
+				Instantiate(propSelectionPrefab,
+					new Vector3((propSelectionOrigin.x + (1 * i)), propSelectionOrigin.y, propSelectionOrigin.z), Quaternion.identity);
 
 			}
 
