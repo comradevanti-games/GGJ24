4efbe694ee0525ba99ea94c0b6951718eb01d4b4
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 13:06:20 +0100

    feat: load and start act
    commit 1a780e27624e0978dd3b733d064c28250388c4ba
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 13:06:14 2024 +0100
    
        feat: load act and start act
    
    commit 6797accb7afdda7a72eb2ec04680ed0b7c42f4e0
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 13:03:17 2024 +0100
    
        feat: add act 0
    
        Just an empty act asset for now to allow for testing

--- Assets/Code/Runtime/ActKeeper.cs
+++ Assets/Code/Runtime/ActKeeper.cs
@@ -8,5 +8,34 @@ namespace Dev.ComradeVanti.GGJ24
     public class ActKeeper : MonoBehaviour, IActKeeper
     {
         public event Action<IActKeeper.ActChangedArgs>? ActChanged;
+
+
+        private void StartAct(IAct act)
+        {
+            ActChanged?.Invoke(new IActKeeper.ActChangedArgs(act));
+        }
+
+        private async void LoadAndStartAct(int actIndex)
+        {
+            var act = await ActIO.TryLoadAsync(actIndex);
+            if (act == null)
+            {
+                // TODO: Handle act not existing. Transition to next scene?
+                throw new Exception($"Act {actIndex} not found!");
+            }
+
+            StartAct(act);
+        }
+
+        private void LoadAndStartCurrentAct()
+        {
+            var actIndex = ActIndexPersistence.Get();
+            LoadAndStartAct(actIndex);
+        }
+
+        private void Start()
+        {
+            LoadAndStartCurrentAct();
+        }
     }
 }
