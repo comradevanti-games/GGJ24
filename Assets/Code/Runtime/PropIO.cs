d0eb8b1796a83dfeef5e98da305a9658cb2dbeb4
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 15:45:48 +0100

    feat: add load all props stub

---
+++ Assets/Code/Runtime/PropIO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Dev.ComradeVanti.GGJ24
+{
+    /// <summary>
+    /// Contains functions for loading <see cref="IProp"/> objects.
+    /// </summary>
+    public static class PropIO
+    {
+        /// <summary>
+        /// Loads all props.
+        /// </summary>
+        public static Task<IProp> LoadAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
