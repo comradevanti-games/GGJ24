eb6b36ae11039f080115b9ac0372b4dc36c77143
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 16:41:00 +0100

    feat: reset live inventory on setup phase start

--- Assets/Code/Runtime/InventoryKeeper.cs
+++ Assets/Code/Runtime/InventoryKeeper.cs
@@ -1,6 +1,7 @@
 ﻿#nullable enable
 
 using System;
+using Dev.ComradeVanti.GGJ24.Player;
 using UnityEngine;
 
 namespace Dev.ComradeVanti.GGJ24
@@ -19,6 +20,22 @@ namespace Dev.ComradeVanti.GGJ24
         public void ModifyStoredInventory(Func<Inventory, Inventory> updateF)
         {
             StoredInventory = updateF(StoredInventory);
+        }
+
+        private void ResetLiveInventory()
+        {
+            LiveInventory = StoredInventory;
+            LiveInventoryChanged?.Invoke(
+                new IInventoryKeeper.InventoryChangedArgs(LiveInventory));
+        }
+
+        private void Awake()
+        {
+            Singletons.Require<IPhaseKeeper>().PhaseChanged += args =>
+            {
+                if (args.NewPhase == PlayerPhase.Setup)
+                    ResetLiveInventory();
+            };
         }
     }
 }
