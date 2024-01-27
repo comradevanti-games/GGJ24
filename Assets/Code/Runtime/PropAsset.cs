20b0851afaa67181c9458b9071e3b6c35332c2c9
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 13:37:18 +0100

    feat: create prop assets

--- Assets/Code/Runtime/PropAsset.cs
+++ Assets/Code/Runtime/PropAsset.cs
@@ -5,6 +5,7 @@ namespace Dev.ComradeVanti.GGJ24
     /// <summary>
     /// An asset that contains data for a pre-made prop
     /// </summary>
+    [CreateAssetMenu(menuName = "GGJ24/Prop", fileName = "New prop")]
     public class PropAsset : ScriptableObject, IProp
     {
         [SerializeField] private GameObject prefab;
