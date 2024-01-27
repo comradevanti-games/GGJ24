bef89049c191a5ddc388b631e20e7ad34db44ba1
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 14:10:24 +0100

    feat: enter setup mode on act change

--- Assets/Code/Runtime/PhaseKeeper.cs
+++ Assets/Code/Runtime/PhaseKeeper.cs
@@ -1,6 +1,7 @@
 ﻿#nullable enable
 
 using System;
+using Dev.ComradeVanti.GGJ24.Player;
 using UnityEngine;
 
 namespace Dev.ComradeVanti.GGJ24
@@ -8,5 +9,22 @@ namespace Dev.ComradeVanti.GGJ24
     public class PhaseKeeper : MonoBehaviour, IPhaseKeeper
     {
         public event Action<IPhaseKeeper.PhaseChangedArgs>? PhaseChanged;
+
+
+        private void EnterSetupMode()
+        {
+            PhaseChanged?.Invoke(
+                new IPhaseKeeper.PhaseChangedArgs(PlayerPhase.Setup));
+        }
+
+        private void OnActChanged(IActKeeper.ActChangedArgs _)
+        {
+            EnterSetupMode();
+        }
+
+        private void Awake()
+        {
+            Singletons.Require<IActKeeper>().ActChanged += OnActChanged;
+        }
     }
 }
