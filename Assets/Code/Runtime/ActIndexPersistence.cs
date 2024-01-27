fb854f797709812e0c696ca055fa5d3f756d559e
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 12:30:28 +0100

    feat: add act-index persistence

---
+++ Assets/Code/Runtime/ActIndexPersistence.cs
@@ -0,0 +1,27 @@
+﻿#nullable enable
+
+using System;
+using UnityEngine;
+
+namespace Dev.ComradeVanti.GGJ24
+{
+    /// <summary>
+    /// Contains logic for interacting with the persisted act-index.
+    /// </summary>
+    public static class ActIndexPersistence
+    {
+        private const string PlayerPrefKey = "ActIndex";
+
+        private const int FirstIndex = 0;
+
+
+        public static int Get() =>
+            PlayerPrefs.GetInt(PlayerPrefKey, FirstIndex);
+
+        public static void Set(int actIndex) =>
+            PlayerPrefs.SetInt(PlayerPrefKey, actIndex);
+
+        public static void Reset() =>
+            Set(0);
+    }
+}
