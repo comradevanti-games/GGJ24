b0d00573f045c6debef299b85913efd70ccd6326
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-27 18:40:13 +0100

    fix async error

--- Assets/Code/Runtime/PropIO.cs
+++ Assets/Code/Runtime/PropIO.cs
@@ -3,23 +3,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Dev.ComradeVanti.GGJ24
-{
-    /// <summary>
-    /// Contains functions for loading <see cref="IProp"/> objects.
-    /// </summary>
-    public static class PropIO
-    {
-        private const string Path = "Props";
+namespace Dev.ComradeVanti.GGJ24 {
 
+	/// <summary>
+	/// Contains functions for loading <see cref="IProp"/> objects.
+	/// </summary>
+	public static class PropIO {
 
-        /// <summary>
-        /// Loads all props.
-        /// </summary>
-        public static Task<IImmutableSet<IProp>> LoadAllAsync() =>
-            Task.Run(() =>
-                Resources.LoadAll<PropAsset>(Path)
-                         .Cast<IProp>()
-                         .ToImmutableHashSet() as IImmutableSet<IProp>);
-    }
+		private const string Path = "Props";
+
+		/// <summary>
+		/// Loads all props.
+		/// </summary>
+		public static IImmutableSet<IProp> LoadAll() =>
+			Resources.LoadAll<PropAsset>(Path)
+				.Cast<IProp>()
+				.ToImmutableHashSet();
+
+	}
+
 }
