585653acbbcd562778312b609b1f75aee8fca05a
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 11:06:03 +0100

    Squashed commit of the following:
    commit ae7768b00f024393e4f7e137e3e41e9ec9fb63c3
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 11:05:58 2024 +0100
    
        docs: add docs
    
    commit 551f0387e0a701a4c470b24197116aec7e46874b
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 11:04:08 2024 +0100
    
        feat: singleton query
    
    commit 02a2405844ce2b993c3433308d6bfedcf31492e0
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 10:58:49 2024 +0100
    
        feat: singleton tag

---
+++ Assets/Code/Runtime/Singletons.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+
+using System;
+using UnityEngine;
+
+namespace Dev.ComradeVanti.GGJ24
+{
+    /// <summary>
+    /// Utility functions for finding singleton components in the current scene.
+    /// Such components must be on a game-object tagged "Singletons".
+    /// There may only be one such game-object per scene.
+    /// </summary>
+    public static class Singletons
+    {
+        private static GameObject TryGetSingletonHost()
+        {
+            var host = GameObject.FindWithTag("Singletons");
+            if (!host) throw new NullReferenceException("No singleton host in the current scene!");
+            return host;
+        }
+
+        /// <summary>
+        /// Attempts to find a singleton of a specific type. Null if not found.
+        /// </summary>
+        public static T? TryFind<T>() where T : MonoBehaviour =>
+            TryGetSingletonHost().GetComponent<T>();
+
+        /// <summary>
+        /// Attempts to find a singleton of a specific type. 
+        /// </summary>
+        /// <exception cref="NullReferenceException">
+        /// Thrown if singleton is not found.
+        /// </exception>
+        public static T Require<T>() where T : MonoBehaviour
+        {
+            var singleton = TryFind<T>();
+            if (!singleton) throw new NullReferenceException($"Singleton of type {typeof(T).Name} was required but not found.");
+            return singleton!;
+        }
+    }
+}
