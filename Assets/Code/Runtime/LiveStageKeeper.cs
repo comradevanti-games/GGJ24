848a8af12c60a33bd50ad253f7a5f62ab27642a4
ComradeVanti <kingvanti650@gmail.com>
2024-01-27 12:07:00 +0100

    feat: implement live stage-keeper
    commit 05bf2cd26af37c60785f9c8babb6e46c8bf1e6e6
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 12:06:41 2024 +0100
    
        feat: implement live-stage-keeper
    
    commit 6de311804b18a7ba0224ef3dde31c12733d4cda7
    Author: ComradeVanti <kingvanti650@gmail.com>
    Date:   Sat Jan 27 12:02:36 2024 +0100
    
        feat: add live-stage-keeper script

---
+++ Assets/Code/Runtime/LiveStageKeeper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Dev.ComradeVanti.GGJ24
+{
+    public class LiveStageKeeper : MonoBehaviour, ILiveStageKeeper
+    {
+        /// <summary>
+        /// How many units/meters are in one slot.
+        /// </summary>
+        private const float UnitsPerSlot = 1;
+
+        /// <summary>
+        /// The direction in which the slots are laid out on the stage.
+        /// </summary>
+        private readonly Vector3 stageDirection = Vector3.right;
+
+
+        [SerializeField] private Transform firstSlotTransform;
+
+        public Vector3? TryGetPositionForSlot(int slotIndex) =>
+            firstSlotTransform.position + stageDirection * UnitsPerSlot;
+    }
+}
