56251e8c46fd49857123995feb0a32c5c79fbf0d
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-27 14:05:17 +0100

    add act initialization on play press

--- Assets/Code/Runtime/ActKeeper.cs
+++ Assets/Code/Runtime/ActKeeper.cs
@@ -3,39 +3,31 @@
 using System;
 using UnityEngine;
 
-namespace Dev.ComradeVanti.GGJ24
-{
-    public class ActKeeper : MonoBehaviour, IActKeeper
-    {
-        public event Action<IActKeeper.ActChangedArgs>? ActChanged;
+namespace Dev.ComradeVanti.GGJ24 {
 
+	public class ActKeeper : MonoBehaviour, IActKeeper {
 
-        private void StartAct(IAct act)
-        {
-            ActChanged?.Invoke(new IActKeeper.ActChangedArgs(act));
-        }
+		public event Action<IActKeeper.ActChangedArgs>? ActChanged;
 
-        private async void LoadAndStartAct(int actIndex)
-        {
-            var act = await ActIO.TryLoadAsync(actIndex);
-            if (act == null)
-            {
-                // TODO: Handle act not existing. Transition to next scene?
-                throw new Exception($"Act {actIndex} not found!");
-            }
+		private void StartAct(IAct act) {
+			ActChanged?.Invoke(new IActKeeper.ActChangedArgs(act));
+		}
 
-            StartAct(act);
-        }
+		private async void LoadAndStartAct(int actIndex) {
+			var act = await ActIO.TryLoadAsync(actIndex);
+			if (act == null) {
+				// TODO: Handle act not existing. Transition to next scene?
+				throw new Exception($"Act {actIndex} not found!");
+			}
 
-        private void LoadAndStartCurrentAct()
-        {
-            var actIndex = ActIndexPersistence.Get();
-            LoadAndStartAct(actIndex);
-        }
+			StartAct(act);
+		}
 
-        private void Start()
-        {
-            LoadAndStartCurrentAct();
-        }
-    }
+		public void LoadAndStartCurrentAct() {
+			var actIndex = ActIndexPersistence.Get();
+			LoadAndStartAct(actIndex);
+		}
+
+	}
+
 }
