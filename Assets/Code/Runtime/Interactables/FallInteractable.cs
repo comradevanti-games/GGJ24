cdbc2277c5225e4e3f97aeebb77528e8bf815663
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-28 13:00:52 +0100

    fix phase switching when no props selected

--- Assets/Code/Runtime/Interactables/FallInteractable.cs
+++ Assets/Code/Runtime/Interactables/FallInteractable.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Dev.ComradeVanti.GGJ24 {
@@ -7,7 +5,6 @@ namespace Dev.ComradeVanti.GGJ24 {
 	public class FallInteractable : MonoBehaviour, IPropInteractable {
 
 		public PropInteraction TryInteraction(PlayerState playerState) {
-			
 			throw new System.NotImplementedException();
 		}
 
