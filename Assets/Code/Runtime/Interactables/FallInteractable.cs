a522cd91a853febc5785010cd290a3b8d599a3e9
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-28 12:54:48 +0100

    add fallinteractable

---
+++ Assets/Code/Runtime/Interactables/FallInteractable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dev.ComradeVanti.GGJ24 {
+
+	public class FallInteractable : MonoBehaviour, IPropInteractable {
+
+		public PropInteraction TryInteraction(PlayerState playerState) {
+			
+			throw new System.NotImplementedException();
+		}
+
+	}
+
+}
