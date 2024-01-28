c9a4b2c207848011bd5e9d7f3ddadad0b331640c
ComradeVanti <kingvanti650@gmail.com>
2024-01-28 13:16:36 +0100

    feat: highlight selected live inventory item

--- Assets/Code/Runtime/InventoryKeeper.cs
+++ Assets/Code/Runtime/InventoryKeeper.cs
@@ -8,32 +8,40 @@ namespace Dev.ComradeVanti.GGJ24
 {
     public class InventoryKeeper : MonoBehaviour, IInventoryKeeper
     {
-        public event Action<IInventoryKeeper.InventoryChangedArgs>? StoredInventoryChanged;
-        public event Action<IInventoryKeeper.InventoryChangedArgs>? LiveInventoryChanged;
+        public event Action<IInventoryKeeper.StoredInventoryChangedArgs>? StoredInventoryChanged;
+        public event Action<IInventoryKeeper.LiveInventoryChangedArgs>? LiveInventoryChanged;
+
+
+        private Inventory liveInventory = Inventory.Empty;
 
 
         public Inventory StoredInventory { get; private set; } = Inventory.Empty;
 
-        public Inventory LiveInventory { get; private set; } = Inventory.Empty;
-
-
-        public void ModifyStoredInventory(Func<Inventory, Inventory> updateF)
+        private Inventory LiveInventory
         {
-            StoredInventory = updateF(StoredInventory);
-            LiveInventory = StoredInventory;
-
-            StoredInventoryChanged?.Invoke(
-                new IInventoryKeeper.InventoryChangedArgs(StoredInventory));
-            LiveInventoryChanged?.Invoke(
-                new IInventoryKeeper.InventoryChangedArgs(LiveInventory));
+            get => liveInventory;
+            set
+            {
+                liveInventory = value;
+                LiveInventoryChanged?.Invoke(
+                    new IInventoryKeeper.LiveInventoryChangedArgs(LiveInventory, 0));
+            }
         }
 
         private void ResetLiveInventory()
         {
             LiveInventory = StoredInventory;
-            LiveInventoryChanged?.Invoke(
-                new IInventoryKeeper.InventoryChangedArgs(LiveInventory));
         }
+
+        public void ModifyStoredInventory(Func<Inventory, Inventory> updateF)
+        {
+            StoredInventory = updateF(StoredInventory);
+            StoredInventoryChanged?.Invoke(
+                new IInventoryKeeper.StoredInventoryChangedArgs(StoredInventory));
+
+            ResetLiveInventory();
+        }
+
 
         private void Awake()
         {
