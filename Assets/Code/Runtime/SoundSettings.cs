f727446cc639c07bf943cccac226a82110375bb1
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-28 16:09:56 +0100

    fix restarting music

--- Assets/Code/Runtime/SoundSettings.cs
+++ Assets/Code/Runtime/SoundSettings.cs
@@ -5,35 +5,36 @@ using Dev.ComradeVanti.GGJ24.Player;
 using UnityEngine;
 using UnityEngine.Audio;
 
-namespace Dev.ComradeVanti.GGJ24
-{
-    public class SoundSettings : MonoBehaviour
-    {
-        public AudioMixer mixer;
-        public AudioSource[] MusicResource;
-        public AudioClip[] OneShots;
-        public void SetLevel(float sliderValue)
-        {
-            mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue)*20 );
-        }
+namespace Dev.ComradeVanti.GGJ24 {
 
-        public void Awake()
-        {
-            Singletons.Require<PhaseKeeper>().PhaseChanged += args =>
-            {
-                if (args.NewPhase == PlayerPhase.Menu)
-                {
-                    MusicResource[0].Play();
-                    MusicResource[1].Pause();
-                }
-                else
-                {
-                    MusicResource[1].Play();
-                    MusicResource[0].Pause();
-                }
-                
-            };
-            
-        }
-    }
+	public class SoundSettings : MonoBehaviour {
+
+		public AudioMixer mixer;
+		public AudioSource[] MusicResource;
+		public AudioClip[] OneShots;
+
+		public void SetLevel(float sliderValue) {
+			mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+		}
+
+		public void Awake() {
+			Singletons.Require<PhaseKeeper>().PhaseChanged += args => {
+				if (args.NewPhase == PlayerPhase.Menu) {
+					MusicResource[0].Play();
+					MusicResource[1].Pause();
+				}
+				else {
+					if (!MusicResource[1].isPlaying) {
+						MusicResource[1].Play();
+					}
+
+					MusicResource[0].Pause();
+				}
+
+			};
+
+		}
+
+	}
+
 }
