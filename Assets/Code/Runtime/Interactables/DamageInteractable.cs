6cfeb7f2610bfa63fda581227127d0e6df04c539
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-28 13:48:33 +0100

    add damage interactable

---
+++ Assets/Code/Runtime/Interactables/DamageInteractable.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Dev.ComradeVanti.GGJ24 {
+
+	public class DamageInteractable : MonoBehaviour, IPropInteractable {
+
+		public PropInteraction TryInteraction(PerformanceState performanceState) {
+			throw new System.NotImplementedException();
+		}
+
+	}
+
+}
