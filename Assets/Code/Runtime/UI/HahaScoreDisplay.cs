15221c65668904e143a73f751926e02879e4a8d5
ComradeVanti <kingvanti650@gmail.com>
2024-01-28 15:55:59 +0100

    feat: show final score

---
+++ Assets/Code/Runtime/UI/HahaScoreDisplay.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+using System;
+using Dev.ComradeVanti.GGJ24.Player;
+using TMPro;
+using UnityEngine;
+
+namespace Dev.ComradeVanti.GGJ24
+{
+    public class HahaScoreDisplay : MonoBehaviour
+    {
+        private TextMeshProUGUI displayText = null!;
+
+
+        private void Awake()
+        {
+            displayText = GetComponentInChildren<TextMeshProUGUI>();
+            Singletons.Require<IPhaseKeeper>().PhaseChanged += args =>
+            {
+                var isVisible = args.NewPhase == PlayerPhase.Eval;
+                gameObject.SetActive(isVisible);
+
+                var score = Singletons.Require<ILiveCrowdKeeper>().CurrentHahaScore;
+                displayText.text = score.ToString();
+            };
+        }
+    }
+}
